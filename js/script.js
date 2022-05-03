8c12e14c48235664c5265cb6ad2a651112e5242f
WashingtonYandun <washington.yandun06@gmail.com>
2022-05-02 23:58:29 -0500

    conect lines

--- js/script.js
+++ js/script.js
@@ -1,12 +1,20 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
-canvas.width = 500;
-canvas.height = 500;
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
-let size = 500 / 100;
+let SIZE = parseInt(canvas.width / 100) + 2;
 
 let grid;
+let oldX = 0;
+let oldY = 0;
+let x = 0;
+let y = 0;
+
+const getRandomInt = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
 
 const makeGrid = (cols, rows) => {
   let matrix = new Array(cols);
@@ -16,5 +24,40 @@ const makeGrid = (cols, rows) => {
   return matrix;
 };
 
-grid = makeGrid(size, size);
+const draw = () => {
+  let direction = getRandomInt(0, 4);
+  oldX = x;
+  oldY = y;
+  switch (direction) {
+    case 0:
+      x += 5;
+      break;
+    case 1:
+      x -= 5;
+      break;
+    case 2:
+      y -= 5;
+      break;
+    case 3:
+      y -= 5;
+      break;
+    default:
+      break;
+  }
+  ctx.lineWidth = 10;
+  ctx.beginPath();
+  ctx.moveTo(oldX, oldY);
+  ctx.lineTo(x, y);
+  ctx.strokeStyle = "#000000";
+  ctx.stroke();
+};
+
+const animate = () => {
+  draw();
+  requestAnimationFrame(animate);
+};
+
+grid = makeGrid(SIZE, SIZE);
 console.log(grid);
+
+animate();
