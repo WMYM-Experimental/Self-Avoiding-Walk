290ba8a3d3e2b13fc0b45ed427cc113bd4f3b16d
WashingtonYandun <washington.yandun06@gmail.com>
2022-06-24 23:13:04 -0500

    trying to finish this

--- js/script.js
+++ js/script.js
@@ -1,9 +1,32 @@
-import { Walker } from "./Walker.js";
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
 canvas.width = 500;
 canvas.height = 500;
+
+let grid = makeMatrix(COLS, ROWS);
+
+class Walker {
+    constructor() {
+        this.x = 0;
+        this.y = 0;
+        this.oldX = 0;
+        this.oldY = 0;
+        this.direction = 0;
+        this.radius = 5;
+    }
+    setDirection() {}
+    checkLimits() {}
+    draw() {
+        ctx.lineWidth = 2;
+        ctx.beginPath();
+        ctx.moveTo(this.oldX * SIZE - SIZE / 2, this.oldY * SIZE - SIZE / 2);
+        ctx.lineTo(this.x * SIZE - SIZE / 2, this.y * SIZE - SIZE / 2);
+        ctx.strokeStyle = "#ffffff";
+        ctx.stroke();
+        ctx.closePath();
+    }
+}
 
 let SIZE = 50;
 
@@ -15,61 +38,27 @@ let oldY = 0;
 const COLS = parseInt(canvas.width / SIZE);
 const ROWS = parseInt(canvas.height / SIZE);
 
-const drawer = new Walker(oldX, oldY);
-
 const allOptions = [
-  { dx: 1, dy: 0 },
-  { dx: -1, dy: 0 },
-  { dx: 0, dy: 1 },
-  { dx: 0, dy: -1 },
+    { dx: 1, dy: 0 },
+    { dx: -1, dy: 0 },
+    { dx: 0, dy: 1 },
+    { dx: 0, dy: -1 },
 ];
 
 const getRandomInt = (min, max) => {
-  return Math.floor(Math.random() * (max - min + 1) + min);
+    return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
 const makeMatrix = (cols, rows) => {
-  let matrix = new Array(cols);
-  for (let i = 0; i < matrix.length; i++) {
-    matrix[i] = new Array(rows);
-  }
-
-  for (let i = 0; i < cols; i++) {
-    for (let j = 0; j < rows; j++) {
-      matrix[i][j] = false;
+    let matrix = new Array(cols);
+    for (let i = 0; i < matrix.length; i++) {
+        matrix[i] = new Array(rows);
     }
-  }
-  return matrix;
+    return matrix;
 };
 
-const isValid = (i, j) => {
-  if (i < 0 || i >= COLS || j < 0 || j >= ROWS) {
-    return false;
-  }
-  return !matrix[i][j];
-};
-
-matrix = makeMatrix(COLS, ROWS);
-let options = [];
-console.log(matrix);
 const animate = () => {
-  for (let i = 0; i < COLS; i++) {
-    for (let j = 0; j < ROWS; j++) {
-      ctx.beginPath();
-      ctx.strokeStyle = "#000000";
-      ctx.rect(i * SIZE, j * SIZE, SIZE, SIZE);
-      ctx.stroke();
-      ctx.closePath();
-    }
-  }
-  let selector = getRandomInt(0, 3);
-  drawer.oldX = drawer.x;
-  drawer.oldY = drawer.y;
-  drawer.setDirection();
-  drawer.draw();
-  requestAnimationFrame(animate);
+    requestAnimationFrame(animate);
 };
 
 animate();
-
-export { getRandomInt, SIZE, ctx, matrix, canvas };
