b926bdf1c5e3161c28e858f704d20101e98952f8
WashingtonYandun <washington.yandun06@gmail.com>
2022-08-06 12:26:22 -0500

    start making it in a small sample of the grid

--- js/Walker.js
+++ js/Walker.js
@@ -1,3 +1,25 @@
 // self avoiding walker class
+import { ctx, canvas } from "./script.js";
 
-class Walker {}
+class Walker {
+    constructor(x, y, direction, color) {
+        this.x = x;
+        this.y = y;
+        this.direction = direction;
+        this.color = color;
+    }
+
+    move() {
+        if (this.direction == 0) {
+            this.x++;
+        } else if (this.direction == 1) {
+            this.x--;
+        } else if (this.direction == 2) {
+            this.y--;
+        } else if (this.direction == 3) {
+            this.y++;
+        }
+    }
+}
+
+export { Walker };
--- js/script.js
+++ js/script.js
@@ -1,10 +1,24 @@
+import { Walker } from "./Walker.js";
+
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
-const CELL_SIZE = 10;
+const CELL_SIZE = 50;
 
-canvas.width = CELL_SIZE * matrix[0].length;
-canvas.height = CELL_SIZE * matrix.length;
+canvas.width = 500;
+canvas.height = 500;
+
+let dim = {
+    x: canvas.width / CELL_SIZE,
+    y: canvas.height / CELL_SIZE,
+};
+
+const allOptions = [
+    { dx: 1, dy: 0 },
+    { dx: -1, dy: 0 },
+    { dx: 0, dy: 1 },
+    { dx: 0, dy: -1 },
+];
 
 const getRandomInt = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1) + min);
@@ -18,18 +32,37 @@ const makeMatrix = (rows, cols) => {
     return matrix;
 };
 
-const drawMatrix = (matrix, color) => {
+const drawMatrix = (matrix, color, space) => {
     matrix.forEach((row, i) => {
         row.forEach((col, j) => {
-            ctx.beginPath();
-            ctx.strokeStyle = color;
-            ctx.rect(j * 10, i * 10, 10, 10);
-            ctx.stroke();
-            ctx.closePath();
+            if (matrix[i][j] === 1) {
+                ctx.beginPath();
+                ctx.strokeStyle = color;
+                ctx.fillStyle = color;
+                ctx.fillRect(i * space, j * space, space, space);
+                ctx.stroke();
+                ctx.closePath();
+            } else {
+                ctx.beginPath();
+                ctx.strokeStyle = color;
+                ctx.rect(i * space, j * space, space, space);
+                ctx.stroke();
+                ctx.closePath();
+            }
         });
     });
 };
 
-let matrix = makeMatrix(50, 50);
+let matrix = makeMatrix(dim.x, dim.y);
 
-drawMatrix(matrix, "green");
+console.log(matrix);
+
+const animate = () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawMatrix(matrix, "#fff", CELL_SIZE);
+    requestAnimationFrame(animate);
+};
+
+animate();
+
+export { ctx, canvas };
